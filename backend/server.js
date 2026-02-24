7c0a681b9db820db112587996ccf3076f4d4a4f3
hrshiti <hritik45raghuwanshi@gmail.com>
2026-02-24 15:15:03 +0530

    new chnages in server linmk

--- backend/server.js
+++ backend/server.js
@@ -52,11 +52,12 @@ app.use(compression());
 
 // CORS configuration (MUST BE BEFORE LIMITER)
 const allowedOrigins = [
-  // 'https://inplay-two.vercel.app',
+
   'http://localhost:5173',
-  // 'http://localhost:3000',
-  // 'https://inplays.in',
-  // 'https://www.inplays.in'
+  'https://riddo-tv.vercel.app',
+  'riddo-tv.vercel.app',
+  'www.riddo-tv.vercel.app',
+
 ];
 
 if (process.env.FRONTEND_URL) {
